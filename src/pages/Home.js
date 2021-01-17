d9f24ac2dd55871466fbbd4b04c2dd348b15400b
dodong <qkrdkwl9090@naver.com>
2021-01-17 22:11:58 +0900

    그리드 내용추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../scss/home.scss";
 import Chart from "react-apexcharts";
-
+import bit from "../images/bitcoin.svg";
 function home(){
     const state =   {
         options: {
@@ -21,23 +21,59 @@ function home(){
           }
         ]
       };
+
+      const state2 = {
+        options: {},
+        series: [44, 55, 41, 17, 15],
+        labels: ['A', 'B', 'C', 'D', 'E']
+      }
     return(
         <div className="home-wrap">
-            <div className="topCon myWallet itemChild1"></div>
-            <div className="topCon itemChild2"></div>
-            <div className="topCon itemChild3"></div>
-            <div className="topCon itemChild4"></div>
-            <div className="chartCon">
-                <Chart
-                    options={state.options}
-                    series={state.series}
-                    width="700"
-                    height="300"
-                    type="line"       
-                    color={state.color}  
-                />
+          <div className="grid-wrap">
+              <div className="topCon myWallet itemChild1">
+                <div className="walletCon">
+                    내 자산 현황
+                    <div className="coutour"></div>
+                    <div className="price">300,000원</div>
+                </div>
+              </div>
+              <div className="topCon itemChild2">
+                <div className="walletCon">
+                    관심 종목
+                    <div className="coutour"></div>
+                    <div className="price">300,000원</div>
+                </div>
+              </div>
+              <div className="topCon itemChild3"></div>
+              <div className="topCon itemChild4"></div>
+              <div className="chartCon">
+                  <Chart
+                      options={state.options}
+                      series={state.series}
+                      width="900"
+                      height="300"
+                      type="line"       
+                      color={state.color}  
+                  />
+              </div>
+              <div className="chartCon2">
+                  <Chart
+                      options={state2.options}
+                      series={state2.series}
+                      width="400"
+                      height="300"
+                      type="donut"       
+                      
+                  />
+              </div>
+              <div className="rank">
+asdad
+              </div>
+              
+              <div className="rank2">
+asd
+              </div>
             </div>
-
         </div>
     )
 }
